e174c0d5ca0e54762464a432e6700451115478a5
CoryWilk <123584905+CoryWilk@users.noreply.github.com>
2024-05-31 21:38:33 -0500

    Getting user input

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,47 +2,32 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-//
-//        System.out.println("You are walking in the forest.");
-//
-//        String myWeapon;
-//        String myArmor;
-//
-//        myWeapon = "Axe";
-//        myArmor = "Leather Tunic";
-//
-//        System.out.println("Your current weapon is " + myWeapon);
-//        System.out.println("Your current armor is " + myArmor);
-//
-//        int yourHP;
-//        yourHP = 10;
-//
-//        System.out.println("Your current HP is " + yourHP);
-//
-//        System.out.println("A monster attacked you and gave 4 damage!");
-//
-//        yourHP -= 4;
-//
-//        System.out.println("Your current HP is " + yourHP);
+        Game game;
+        game = new Game();
+        game.playerSetUp();
+
+    }
+
+    public void playerSetUp(){
+        int playerHP;
+        playerHP = 15;
+
+        String playerWeapon;
+        playerWeapon = "Knife";
+
+        System.out.println("Your HP: " + playerHP);
+        System.out.println("Your weapon: " + playerWeapon);
+
+        String playerName;
 
         Scanner scanner;
         scanner = new Scanner(System.in);
 
-//        String playerName;
-//
-//        System.out.println("Please enter your name:");
-//
-//        playerName = scanner.nextLine();
-//
-//        System.out.println("Hello " + playerName);
+        System.out.println("Please enter your name:");
+        playerName = scanner.nextLine();
 
-        int number;
-
-        System.out.println("Please enter a number:");
-
-        number = scanner.nextInt();
-
-        System.out.println("The number you typed is " + number);
+        System.out.println("Hello " + playerName + ", let's start your adventure!");
 
     }
+
 }
